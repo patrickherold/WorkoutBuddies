aca6452f43927f4a2545ba4d6a5f613177f4bbbe
patrickherold <patrick@herold.org>
2019-01-05 17:47:37 -0500

    files added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # workoutBuddies
 A simple JS app to help keep the fitness fun. Built by the guys at Group 3. 
+
+https://docs.google.com/document/d/1jKlksx5oxu0AxXN5WrHN6-Bx1lXOlEMz6nT_FeuegFs/edit?usp=sharing
---
+++ js/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyD-Rni68sV7NSl1a_nRI5QtDZ-GazqCcYg",
+    authDomain: "workoutbuddies-a9eb5.firebaseapp.com",
+    databaseURL: "https://workoutbuddies-a9eb5.firebaseio.com",
+    projectId: "workoutbuddies-a9eb5",
+    storageBucket: "workoutbuddies-a9eb5.appspot.com",
+    messagingSenderId: "25982813153"
+  };
+
+  firebase.initializeApp(config);
+
