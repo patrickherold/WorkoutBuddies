c7f812507fb190a9e51e61c43eb9bc0634fde648
patrickherold <patrick@herold.org>
2019-01-05 18:43:17 -0500

    added some outlines to the pages

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// firebase setup and config
+
 var config = {
     apiKey: "AIzaSyD-Rni68sV7NSl1a_nRI5QtDZ-GazqCcYg",
     authDomain: "workoutbuddies-a9eb5.firebaseapp.com",
@@ -8,4 +10,29 @@ var config = {
   };
 
   firebase.initializeApp(config);
+
+
+
+//  Landing page 
+
+// Default welcome with:
+
+  //  Login / logout
+
+  
+
+  // navigation that connects to:
+
+    // user profile
+
+    // map view
+
+    // workout??
+
+    // other?  possibly messages or connections??
+
+
+// google analytics??
+
+
 
