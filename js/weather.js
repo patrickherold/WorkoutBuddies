e9480ee0f9d9feefc24bf8a42fb9d7f1bda681e1
Reese Fairbanks <reese@Reeses-MacBook-Pro.local>
2019-01-12 14:07:02 -0500

    Working on address to lat long

---
+++ js/weather.js
@@ -0,0 +1,49 @@
+var config = {
+    apiKey: "AIzaSyD-Rni68sV7NSl1a_nRI5QtDZ-GazqCcYg",
+    authDomain: "workoutbuddies-a9eb5.firebaseapp.com",
+    databaseURL: "https://workoutbuddies-a9eb5.firebaseio.com",
+    projectId: "workoutbuddies-a9eb5",
+    storageBucket: "workoutbuddies-a9eb5.appspot.com",
+    messagingSenderId: "25982813153"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var user
+
+//  THIS IS NEEDED TO GET THE CURRENT USER. AND BECAUSE WE HAVE TO WAIT FOR A REPLY FROM THE DATABASE
+firebase.auth().onAuthStateChanged( user => {
+    if (user) {
+        var userId = firebase.auth().currentUser.uid;
+        var userSnap = firebase.database().ref('users/' + userId);
+        var address = userSnap.val().address;
+        var geoURL = "https://api.opencagedata.com/geocode/v1/json?key=000be1b151fb4863a869b6bc420760eb&pretty=1&q=" + address;
+        console.log("address" + userSnap.val().address);
+    
+        var lat = "";
+        var lon = "";
+        //Ajax call to get the weather results using geoURL
+        $.ajax({
+            url: geoURL,
+            method: "GET"
+        }).then(function(response) {
+            lat = response.results.geometry.lat;
+            lon = response.results.geometry.lat;
+
+            var weatherURL = "https://cors-anywhere.herokuapp.com/" + "https://api.darksky.net/forecast/7d164bcd822a2ece7033b1a27d4a6e4e/" + lat + "," + lon;
+            console.log(lat);
+            console.log(lon);
+            //ajax call to use the response
+            $.ajax({
+                url: weatherURL,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+            });
+    
+        });
+    
+    }
+    else {
+        console.log("no user yet")
+    }
+});
